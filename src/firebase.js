34614921460d6527cb6e864964a3caa27f7ec3c9
GerardoRosas <rosas.ruizlg@gmail.com>
2020-09-09 06:10:44 -0500

    Initial & nav bar

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC03cRX1WIdJovPW1cGQDXQXPXWcPx0Y3I",
+    authDomain: "clone-ea7b0.firebaseapp.com",
+    databaseURL: "https://clone-ea7b0.firebaseio.com",
+    projectId: "clone-ea7b0",
+    storageBucket: "clone-ea7b0.appspot.com",
+    messagingSenderId: "481465384287",
+    appId: "1:481465384287:web:dc3ce80e8a6706d24a4aea"
+  };
